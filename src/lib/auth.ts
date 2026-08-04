5c84120f222fe3ea74adfe851c09a1f2734198c9
fahmolanrewaju-prog <fahmolanrewaju@gmail.com>
2026-08-04 23:49:05 +0100

    Remove incorrect import

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,12 +2,10 @@ import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { PrismaClient } from "@prisma/client";
 import { nextCookies } from "better-auth/next-js";
-import { createAuthMiddleware } from "better-auth";
 
 const prisma = new PrismaClient();
 
 export const auth = betterAuth({
-  // We use a function to properly initialize the adapter
   database: () => prismaAdapter(prisma),
   emailAndPassword: {
     enabled: true,
