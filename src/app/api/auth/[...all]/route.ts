91baff1709d347d7bb70b780e5259fc5d491cb06
fahmolanrewaju-prog <fahmolanrewaju@gmail.com>
2026-08-05 07:07:39 +0100

    Add missing toNextJsHandler import

--- src/app/api/auth/[...all]/route.ts
+++ src/app/api/auth/[...all]/route.ts
@@ -1,4 +1,4 @@
-import { NextResponse } from 'next/server';
-import { prisma } from '@/lib/auth'; // <--- CHANGED THIS LINE
+import { auth } from "@/lib/auth";
+import { toNextJsHandler } from "better-auth/next-js";
 
 export const { GET, POST } = toNextJsHandler(auth);
